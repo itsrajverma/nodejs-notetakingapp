5815dec2df0862a268f499847900268e934f209c
Raj Verma <info.mrverma@gmail.com>
2019-08-11 18:43:21 +0530

    Notes app with Add,Read,List,Remove functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const yargs = require('yargs');
+const yargs = require('yargs'); 
 const notes = require('./notes');
 
+
+// Command for add a new note
 yargs.command({
     command:'add',
     describe:'Add a new note',
@@ -21,6 +23,7 @@ yargs.command({
     }
 });
 
+// Command for remove a  note
 yargs.command({
     command:'remove',
     describe:'Remove a note',
@@ -32,11 +35,34 @@ yargs.command({
         }
     },
     handler:function(argv){
-        console.log(argv.title);
+        notes.removeNote(argv.title);
     }
 });
 
+// Command for read a  note
+yargs.command({
+    command:'read',
+    describe:'Read a note',
+    builder:{
+        title:{
+            describe:'Enter the title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(argv){
+        notes.readNote(argv.title);
+    }
+});
 
+// Command for list all notes
+yargs.command({
+    command:'list',
+    describe:'list all notes',
+    handler:function(){
+        notes.listNotes();
+    }
+})
 
 
 yargs.parse();
--- notes.js
+++ notes.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const chalk = require('chalk');
 const log = console.log;
 
-
+// Function for Add a notes
 const addNotes = function(title,body){
+
+    // First Load Old Notes
     const notes = loadNotes(); 
 
+    // Check old Notes with passed title for remove duplicacy
     const duplicatenotes = notes.filter(function(note){
         return note.title === title
     });
@@ -22,12 +25,56 @@ const addNotes = function(title,body){
     }
 }
 
+
+// Function for Save notes in notes.json file
 const saveNotes = function(notes){
     const data = JSON.stringify(notes);
     fs.writeFileSync('notes.json',data);
 }
 
+// Function for list all notes
+const listNotes = function(){
+    const notes = loadNotes();
+    notes.forEach(function(note){
+        log(chalk.blue(note.title));
+    })
+}
 
+// Function for Remove a note
+const removeNote = function(title){
+    const notes = loadNotes();
+    // Store Notes in new variable thats we do not want to delete
+    const notesToKeep = notes.filter(function(note){
+        return note.title!==title
+    })
+
+    if(notes.length > notesToKeep.length){
+        log(chalk.bgGreen.bold('Removed successfully...'));
+        saveNotes(notesToKeep);
+    }else{
+        log(chalk.bgRed.bold('No record found..'));
+    }
+
+}
+
+// Function for Read a note
+const readNote = function(title){
+    const notes = loadNotes();
+
+    // Find the notes with title that's user pass
+    const requiredNote = notes.find(function(note){
+        return note.title===title
+    })
+    if(requiredNote){
+        log('Title : ' + chalk.blue(requiredNote.title));
+        log('Body : ' + chalk.blue(requiredNote.body));
+    }else{
+        log(chalk.bgRed.bold('No record found..'));
+    }
+}
+
+
+// Function for Load all notes in Json Format
 const loadNotes = function(){
     try{
         const databuffer = fs.readFileSync('notes.json');
@@ -39,6 +86,11 @@ const loadNotes = function(){
     }
 }
 
+
+// Export functions 
 module.exports = {
-    addNote: addNotes
+    addNote: addNotes,
+    listNotes:listNotes,
+    removeNote:removeNote,
+    readNote:readNote
 }
